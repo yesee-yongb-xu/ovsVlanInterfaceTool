29de1de943770ff6f552b34f592050b0df7aeef0
yesee-yongb-xu <91236821+yesee-yongb-xu@users.noreply.github.com>
2023-07-12 15:50:33 +0800

    Update add_vlan_interface.sh

--- add_vlan_interface.sh
+++ add_vlan_interface.sh
@@ -2,8 +2,8 @@
 
 for ((i=1001; i<=1050; i++))
 do
-    echo "TYPE=\"Vlan\""
-    echo "PHYSDEV=\"eth1\""
-    echo "VLAN_ID=\"$i\""
+    echo "auto eth0.$i"
+    echo "iface eth0.$i inet static"
+    echo "    vlan-raw-device eth0"
     echo
 done
