c0820edf76842c92a2241d1ca39d65552e138450
yesee-yongb-xu <yongb.xu@yesee.com.tw>
2023-07-13 09:21:59 +0800

    改為參數輸入

--- add_vlan_bridges.sh
+++ add_vlan_bridges.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-for ((i=1001; i<=1050; i++))
+CODE=$1
+DEVICE=$2
+VLAN_START=$3
+VLAN_END=$4
+
+if [[ -z $CODE || -z $DEVICE  || -z $VLAN_START || -z $VLAN_END ]]; then
+  echo "Missing arguments. Usage: bash add_vlan_bridges.sh <CODE> <DEVICE> <VLAN START> <VLAN END>"
+  exit 1
+fi
+
+for ((i=$VLAN_START; i<=$VLAN_END; i++))
 do
-    ovs-vsctl add-br vlan-"$i"-br
-    ovs-vsctl add-port vlan-"$i"-br eth1."$i"
+    bridge="vlan-${i}-${CODE}"
+    subinterface="${DEVICE}.${i}"
+
+    echo "Adding bridge $bridge..."
+    ovs-vsctl add-br $bridge
+
+    echo "Adding port $subinterface to bridge $bridge..."
+    ovs-vsctl add-port $bridge $subinterface
 done
+
--- centos_add_vlan_interface.sh
+++ centos_add_vlan_interface.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-INTERFACE="eth1"
-START_VLAN_ID=1001
-NUM_VLANS=50  
+DEVICE=$1
+VLAN_START=$2
+VLAN_END=$3  
 
-for ((i = 0; i < NUM_VLANS; i++))
+
+if [[  -z $DEVICE  || -z $VLAN_START || -z $VLAN_END ]]; then
+  echo "Missing arguments. Usage: bash centos_add_vlan_interface.sh <DEVICE> <VLAN START> <VLAN END>"
+  exit 1
+fi
+
+
+for ((i = $VLAN_START; i <= $VLAN_END; i++))
 do
-  VLAN_ID=$((START_VLAN_ID + i))
+  VLAN_ID=$i
 
  
-  CONFIG_FILE="/etc/sysconfig/network-scripts/ifcfg-${INTERFACE}.${VLAN_ID}"
+  CONFIG_FILE="/etc/sysconfig/network-scripts/ifcfg-${DEVICE}.${VLAN_ID}"
   echo "TYPE=\"Vlan\"" > "$CONFIG_FILE"
   echo "PHYSDEV=\"${INTERFACE}\"" >> "$CONFIG_FILE"
   echo "VLAN_ID=\"${VLAN_ID}\"" >> "$CONFIG_FILE"
